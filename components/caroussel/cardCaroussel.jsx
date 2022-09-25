7d8e9f0da70d3cbb8e609e99f027ca09c1e0d055
youssef-saidi <78275450+youssef-saidi@users.noreply.github.com>
2022-09-25 14:51:38 +0200

    Update cardCaroussel.jsx

--- components/caroussel/cardCaroussel.jsx
+++ components/caroussel/cardCaroussel.jsx
@@ -5,13 +5,14 @@ import Link from 'next/link'
 
 
 const CardCaroussel = ({ url, img }) => {
+    console.log(url)
     return (
         <div className="z-10 flex items-center justify-center cardHover z-10">
             <Image src={img} height={200} width={400} />
             <div className='middle z-10'>
-                <Link href={url}>
+                <a href={url} target={"_blank"}  rel="noreferrer" >
                     <Button title="Link" color={"#F5F5F5"} bgcolor={"#232C33"} fill={"#2E90E1"} />
-                </Link>
+                </a>
             </div>
         </div>
 
